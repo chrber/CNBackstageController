506b0bcc254c107dd18a44834fa716548e6bff81
Frank Gregor <phranck@cocoanaut.com>
2012-12-09 23:37:23 +0100

    Changed: support for retina devices

--- CNBackstageController/CNBackstageController.m
+++ CNBackstageController/CNBackstageController.m
@@ -792,7 +792,8 @@
 
 - (CGImageRef)snapshotOfDisplayWithID:(CGDirectDisplayID)displayID
 {
-    return CGDisplayCreateImageForRect(displayID, CGRectMake(0, 0 + [self thicknessOfSystemStatusBarForCurrentToggleDisplay],
+    return CGDisplayCreateImageForRect(displayID, CNRectMake([self screenOfCurrentToggleDisplay],
+                                                             0, 0 + [self thicknessOfSystemStatusBarForCurrentToggleDisplay],
                                                              NSWidth(self.currentToggleDisplayFrame),
                                                              NSHeight(self.currentToggleDisplayFrame) - [self thicknessOfSystemStatusBarForCurrentToggleDisplay]));
 }
